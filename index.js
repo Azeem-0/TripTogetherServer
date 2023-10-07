e6b4d3ee38c78a26f9115620bdc8dff01c33407b
Azeem Pasha <azeemshaik025@gmail.com>
2023-10-07 18:34:35 +0530

    Created Basic Template for index.js

--- index.js
+++ index.js
@@ -2,31 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-const authentication = require("./apis/authApi");
 const mongoose = require("mongoose");
-const getDetails = require("./apis/getDetails");
-const { checkTimeValidity, checkAvailableSeats } = require("./controllers/schedularController");
-const cron = require("node-cron");
-const trips = require("./apis/trips");
 const app = express();
 
 
 mongoose.connect(process.env.DB).then(() => {
     console.log("Connected to Database");
 });
-
-cron.schedule('0 2 * * *', () => {
-    checkTimeValidity();
-    checkAvailableSeats();
-});
-
-// MiddleWares
-app.use(cors());
-app.use(express.json());
-app.use("/auth", authentication);
-app.use("/", getDetails);
-app.use("/trips", trips);
-
 
 
 app.listen(port, () => {
